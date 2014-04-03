13cc759bd163c3e94f6e9a2a741432f3e29877be
Amy Rudolph <amy.rudolph@live.co.uk>
2014-04-03 17:44:32 +0400

    changed pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,9 +3,9 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
 	:storage => :s3,
+	:bucket => ENV['pinterestingamy']
     :s3_credentials => {
-      :bucket => ENV['pinterestingamy'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
 end
