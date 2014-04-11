9b8d87ec910274ebbc2ab67f14d515b7c3fefece
Amy Rudolph <amy.rudolph@live.co.uk>
2014-04-11 19:52:53 +0400

    readded new bucket on s3 and changed access key id variable

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,11 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-	:storage => :s3,
-	:s3_credentials => {
-	:bucket => ENV['pinterestingamy']
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
 end
